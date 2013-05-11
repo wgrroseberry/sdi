f8892df53171494aaa3ef046f4d5de7a702b6e22
Willard Roseberry <willardroseberry@Willards-MacBook-Pro.local>
2013-05-11 01:13:59 -0600

    Added first files to the repo.

---
+++ Roseberry_Willard_project_1_save.js
@@ -0,0 +1,14 @@
+// Willard Roseberry
+// Scalable Data Infrastructures
+// This is my 5th month of class.
+
+var say = function(message){console.log(message);};
+
+
+say ("I saved myself " + 8 + " whole characters");
+
+var getArea = function () {say ("getArea fired!");  
+};
+
+
+getArea();
